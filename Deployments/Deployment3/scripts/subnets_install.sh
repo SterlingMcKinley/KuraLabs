3711a60636be9e6832e6729cbeb91a63905f6853
SterlingMcKinley <91057035+SterlingMcKinley@users.noreply.github.com>
2022-10-15 03:25:25 -0500

    script

---
+++ Deployments/Deployment3/scripts/subnets_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download Java Runtime Environment
+sudo apt update && sudo apt install default-jre -y
+
+# Download Python package installer
+sudo apt install python3-pip -y
+
+# Download Py venv
+sudo apt install python3.10-venv -y
+
+# Download Nginx - Linux app for web servers
+sudo apt update && sudo apt install nginx -y 
