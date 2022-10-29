fceebc3e10ceb772016ef5825c0eea493787305f
SterlingMcKinley <91057035+SterlingMcKinley@users.noreply.github.com>
2022-10-29 07:55:24 -0500

    Create jenkins_install.sh

---
+++ Deployments/Deployment4/scripts/jenkins_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Setting the path
+PATH="/var/lib/jenkins/.local/bin:${PATH}"
+
+#Installing python3-venv
+sudo apt-get install python3-venv -y
+
+#Installing pip3
+sudo apt install python3-pip -y
+
+#Installing zip package
+sudo apt-get install zip unzip -y
+
+# Download Java Runtime Environment
+sudo apt update && sudo apt install default-jre -y
+
+# Get the key to access Jenkins package
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo gpg --dearmor -o/usr/share/keyrings/jenkins.gpg
+
+# Using the key to authenticate the package
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+# Update & install Jenkins
+sudo apt update && sudo apt install jenkins -y
+
+# Start Jenkins & check availability status
+sudo systemctl start jenkins
+sudo systemctl status jenkins >> ~/file.txt
