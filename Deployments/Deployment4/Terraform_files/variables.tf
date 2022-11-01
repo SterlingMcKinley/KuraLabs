d16aaa18ba225ee189fe911057790cb4cc650927
SterlingMcKinley <91057035+SterlingMcKinley@users.noreply.github.com>
2022-11-01 14:39:37 -0500

    Add files via upload

---
+++ Deployments/Deployment4/Terraform_files/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {}
+variable "project_name" {}
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
